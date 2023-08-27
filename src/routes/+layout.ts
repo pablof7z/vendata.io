42317ccbe6053e07a55309e2bafc1b821fd17968
Pablo Fernandez <p@f7z.io>
2023-08-27 22:53:19 +0300

    vercel shit

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,7 @@
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
+
 export const prerender = false;
 export const ssr = false;
